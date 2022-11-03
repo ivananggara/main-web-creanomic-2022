cef36e4064d76bbf0e79efc4f44da8eaef228d2d
ivananggara <ivanwanggara30@gmail.com>
2022-11-03 16:11:47 +0700

    first push

---
+++ fest.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include("./partials/head.php");?>
+<body>
+    <?php include("./partials/navbar.php");?>
+    <section id="hero">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1>Hello Fest</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <?php include("./partials/footer.php");?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include("./partials/head.php");?>
+<link rel="stylesheet" href="./assets/css/home.css">
+
+<body>
+<?php include("./partials/navbar.php");?>
+    <section id="hero" class="">
+        <div class="container h-100">
+            <div class="row h-100">
+                <div class="col-12 hero-content my-auto">
+                    <h1>creanomic</h1>
+                    <h2>2022</h2>
+                    <h3>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h3>
+                    <a href="#">Lets start</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id=about>
+        <div class="about">
+            <div class="container">
+                <div class="row">
+                    <div class="col-6 col-sm-12 col-md-12 col-lg-6 about-video">
+                        
+                    </div>
+                    <div class="col-6 col-sm-12 col-md-12 col-lg-6 about-content">
+                        <h1>About Us</h1>
+                        <p>Creative Economy and Innovation Centre, which well-known as Creanomic, is an annual event which holds by Vocational Education Program, Brawijaya University. The event itself brings up topic about creativity and innovation
+                        which maintains the standard economy. <br /><br />Due to corona virus pandemic, the event evolves to adapt the current situation along with bringing new concept this year. Creanomic also brings up new events: Virtual Art
+                        Exhibition, Virtual Concert, Webinar, and widen our competitions scope to International level.</p>
+                        <a href="#">More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php include("./partials/footer.php");?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+</html>
